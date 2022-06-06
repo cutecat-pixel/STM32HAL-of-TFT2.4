f1f431409eadc565a24b287153a633acb5f4a9ec
Cute_CAT <2971504919@qq.com>
2022-06-06 20:59:36 +0800

    Add files via upload

---
+++ TFTCode/Inc/ascii.h
@@ -0,0 +1,7 @@
+#ifndef __ASCII_H__
+#define __ASCII_H__
+extern const unsigned char ascii_1206[][12];
+extern const unsigned char ascii_1608[][16];
+extern const unsigned char ascii_2412[][48];
+extern const unsigned char ascii_3216[][64];
+#endif
